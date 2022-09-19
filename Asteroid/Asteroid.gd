d931ee093a76d0afff5d0bc72baff74fb5868477
GryphonAblaze <112128392+GryphonAblaze@users.noreply.github.com>
2022-09-19 08:37:55 -0400

    I can't fix this

--- Asteroid/Asteroid.gd
+++ Asteroid/Asteroid.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var initial_speed = 3.0
+var health = 1
+var small_asteroids =  [Vector2(0, -30),Vector2(30,30)Vector2(-30,-30)]
+var Asteroid_small = load("res://Asteroid/Asteroid_small.tscn")
 
 func _ready():
 	velocity = Vector2(0,initial_speed*randf()).rotated(PI*2*randf())
@@ -10,3 +13,4 @@ func _physics_process(_delta):
 	position = position + velocity
 	position.x = wrapf(position.x, 0, 1024)
 	position.y = wrapf(position.y, 0, 600)
+
